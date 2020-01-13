00394b75bf7bafdd6339bf6c9a9a1d2a91f4e1ba
sangtm-04 <sangtran.d14ptit@gmail.com>
2020-01-13 22:58:57 +0700

    Up code

---
+++ Demo-NUnit.Test/CalculatorTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_NUnit.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator _cal;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cal = new Calculator();
+        }
+
+        [Test]
+        public void OnePlusOneEqualTwo()
+        {
+            Assert.AreEqual(2, _cal.Add(1, 1));
+        }
+
+        [Test]
+        public void TwoPlusTwoEqualsFour()
+        {
+            Assert.AreEqual(4, _cal.Add(2, 2));
+        }
+
+        [Test]
+        public void FourPlusOneEqualsFive()
+        {
+            Assert.AreEqual(5, _cal.Add(4, 1));
+        }
+    }
+}
